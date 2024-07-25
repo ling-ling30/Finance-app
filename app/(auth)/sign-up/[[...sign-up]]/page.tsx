8479d025e90399c9a8cd16550d673df6008ad19d
Ricky - <rickyjay567@gmail.com>
2024-07-25 21:26:29 +0700

    set up clerk hono middleware

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,30 @@
-import { SignUp } from "@clerk/nextjs";
+import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+import Image from "next/image";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <section className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-[#2E2A47]">Welcome back!</h1>
+          <p className="text-base text-[#7e8ca0]">
+            Log in or Create account to get back to your dashboard!
+          </p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignUp />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 />
+          </ClerkLoading>
+        </div>
+      </section>
+
+      <section className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
+        <Image alt="logo" src="/logo.svg" height={100} width={100} />
+      </section>
+    </div>
+  );
 }
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>test</Button>
+      <UserButton />
+      This is an authenticated route.
     </main>
   );
 }
